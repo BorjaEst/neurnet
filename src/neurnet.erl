d6ba2304016df590c05fc0a9a84a5c890cac3695
BorjaEst <boressan@outlook.com>
2020-04-09 20:50:17 +0200

    Interface simplification and phenotype added

--- src/neurnet.erl
+++ src/neurnet.erl
@@ -11,17 +11,10 @@
 -author("borja").
 -compile([export_all, nowarn_export_all]). %%TODO: To delete after build
 
--include_lib("kernel/include/logger.hrl").
--include_lib("elements.hrl").
 -include_lib("neurnet.hrl").
 
 %% API
 -export([]).
-
--define(MAX_TIME, infinity).
--define(MAX_ATTEMPTS, 20).
--define(FITNESS_TARGET, infinity).
--define(ARCHITECTURE(Layers), enn:sequential(Layers)).
 
 
 %%%===================================================================
@@ -37,99 +30,53 @@
     [{Elem :: nnelements:element(), [Attr :: atom()]}].
 attributes_table() ->
     eevo:attributes_table() ++ enn:attributes_table() ++ [
-        {cortex, genotype:fields(actuator)},
-        {neuron, genotype:fields(sensor  )}
+        % Genotype
+        % Phenotype
+        % Architectures
+
+        {      actuator, actuator:fields(      actuator)},
+        {actuator_group, actuator:fields(actuator_group)},
+        {        sensor,   sensor:fields(        sensor)},
+        {  sensor_group,   sensor:fields(  sensor_group)}
     ].
 
 %%--------------------------------------------------------------------
-%% @doc
-%%
-%%
+%% @doc Loads a module with the genotypes to test in neurnet. 
+%% @end
+%%--------------------------------------------------------------------
+-spec load(GenotypeModules :: [module()]) -> ok.
+load(GenotypeModules) -> 
+    genotype:load(GenotypeModules).
+
+%%--------------------------------------------------------------------
+%% @doc Triggers a genotype per population to evaluate an evolutionary
+%% algorithm with neural networks and returns the score and the id of
+%% the best phenotype for each specified genotype.
 %% @end
 %%--------------------------------------------------------------------
 %TODO: Correct specs
-seed(#{sensors := Sensors, actuators := Actuators} = _Morphology, InitialLayerDensities) ->
-    Cortex_Id = enn:compile(?ARCHITECTURE(
-        lists:append([
-                         [?input(length(Sensors))],
-                         InitialLayerDensities,
-                         [?output(length(Actuators))]
-                     ]))),
-    eevo:create_agent(nn_agent,
-                      ?nn_properties(Sensors, Actuators, Cortex_Id),
-                      fun mutation_function/1
-    ).
+go(Genotypes) -> go(Genotypes, #{}).
+
+%TODO: Correct specs
+go(Genotypes, Options) ->
+    Rules   = maps:merge(?DEFAULT_RULES, Options),
+    Threads = [pop_thread(Rules, Genotype) || Genotype <- Genotypes],
+    [eevo:score_and_champion(Id) || Id <- run_parallel(Threads)}].  
+
+pop_thread(Rules, Genotype) -> 
+    fun()-> eevo:start(Rules, [phenotype(Genotype)]) end.
 
 %%--------------------------------------------------------------------
-%% @doc
-%%
-%%
+%% @doc 
 %% @end
 %%--------------------------------------------------------------------
 %TODO: Correct specs
-start_agent(Agent_Id) ->
-    {ok, _Agent_PId} = eevo:start_agent(?REAL_WORLD_ID, Agent_Id).
-
-%%--------------------------------------------------------------------
-%% @doc
-%%
-%%
-%% @end
-%%--------------------------------------------------------------------
-%TODO: Correct specs
-stop_agent(Agent_Id) ->
-    ok = eevo:stop_agent(?REAL_WORLD_ID, Agent_Id).
-
-%%--------------------------------------------------------------------
-%% @doc
-%%
-%%
-%% @end
-%%--------------------------------------------------------------------
-%TODO: Correct specs
-go(Morphology, InitialLayerDensities) ->
-    go(Morphology, InitialLayerDensities, ?MAX_TIME, ?MAX_ATTEMPTS, ?FITNESS_TARGET).
-go(Morphology, InitialLayerDensities, MaxTime, MaxAttempts, FitnessTarget) ->
-    Seed_Id = seed(Morphology, InitialLayerDensities),
-    Population_Id = eevo:create_population(
-        [
-            {run_time, MaxTime},
-            {run_agents, MaxAttempts},
-            {run_score, FitnessTarget}
-        ]),
-    {ok, _Gov_PId} = eevo:start_population(Population_Id), %% TODO: Make Result = eevo:start_population(Id, Seeds),
-    ok = eevo:add_agent(Population_Id, Seed_Id),
-    Result = receive {run_end, Population_Id, R} -> R end,
-    ?LOG_NOTICE("Training results: ~p", [Result]),
-    {_Score, _Best_Agent_Id} = maps:get(best_score, Result).
-
-
-%%--------------------------------------------------------------------
-%% @doc
-%%
-%%
-%% @end
-%%--------------------------------------------------------------------
-%TODO: Correct specs
-change_sensors(Agent_Id, Sensors) ->
-    Agent = edb:read(Agent_Id),
-    AgentProperties = Agent#agent.properties,
-    edb:write(Agent#agent{
-        properties = AgentProperties#{sensors => Sensors}
-    }).
-
-%%--------------------------------------------------------------------
-%% @doc
-%%
-%%
-%% @end
-%%--------------------------------------------------------------------
-%TODO: Correct specs
-change_actuators(Agent_Id, Actuators) ->
-    Agent = edb:read(Agent_Id),
-    AgentProperties = Agent#agent.properties,
-    edb:write(Agent#agent{
-        properties = AgentProperties#{actuators => Actuators}
+phenotype(Genotype) -> 
+    Phenotype_Id = genotype:buid(Genotype),
+    eevo:agent(#{
+        function  => fun phenotype:controller/1,
+        mutation  => fun phenotype:mutator/1,
+        arguments => Phenotype_Id
     }).
 
 
@@ -137,24 +84,18 @@ change_actuators(Agent_Id, Actuators) ->
 %%% Internal functions
 %%%===================================================================
 
-mutation_function(Properties) ->
-    Cortex_Id = enn:clone(?cortex_id(Properties)),
-    apply_algorithms(?ALGORITHM_APPLY_ORDER, Cortex_Id, ?algorithm(Properties)),
-    Properties#{
-        cortex_id := Cortex_Id,
-        size      := nn_elements:size(edb:read(Cortex_Id))
-    }.
+% --------------------------------------------------------------------
+run_parallel(Functions) -> 
+    Pids = [spawn(request_result(self(), F)) || F <- Functions],
+    receive_results(Pids).
 
-apply_algorithms([FunKey | Functions], Cortex_Id, Algorithm) ->
-    Value = maps:get(FunKey, Algorithm),
-    try
-        algorithm:FunKey(Cortex_Id, Value, Algorithm)
-    catch
-        Type:Exception -> ?LOG_NOTICE("Algorithm application ~p failed: {~p, ~p}", [FunKey, Type, Exception])
-    end,
-    apply_algorithms(Functions, Cortex_Id, Algorithm);
-apply_algorithms([], _Cortex_Id, _Algorithm) ->
-    ok.
+request_result(Parent, Function) -> 
+    Parent ! {self, Function()}.
+
+receive_results([Pid | Pids]) -> 
+    receive {Pid, Result} -> [Result | receive_results(Pids)] end
+
+
 
 
 
