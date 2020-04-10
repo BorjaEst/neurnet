c3cb1b3abea581ed507f1f60a7a7513a5dadfa82
BorjaEst <boressan@outlook.com>
2020-04-10 20:25:21 +0200

    architecture mutations simplified

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Deep learning application based on Erlang and Python modules
 
 # Important
 If developing with parallel applications into release use _checkouts to solve dependencies
+
+# Notes
+[_] - Actuators and sensors can mutate (but following a path):
+A1 -> [A2, A4];
+A2 -> [A1, A3];
+A3 -> [];
+A4 -> [A2];
--- src/architecture.erl
+++ src/architecture.erl
@@ -23,6 +23,7 @@
 
 -define(DEF_DIM,  [3]).
 -define(DEF_TYPE, recurrent).
+-define(MAYBE(Probability), Probability > rand:uniform() andalso).
 
 
 %%%===================================================================
@@ -58,6 +59,96 @@ new_network(A) ->
         model:Nature([layer:dense(N) || N <- A#architecture.dim])
     ).
 
+%%-------------------------------------------------------------------
+%% @doc Mutates a network returning the new mutation id. 
+%% @end
+%%-------------------------------------------------------------------
+-spec mutate(Network_id :: enn:id()) -> 
+    NewNetwork_id :: enn:id().
+mutate(Network_id) -> 
+    Clone_Id = enn:clone(Network_id),
+    mutate_network(edb:read(Clone_Id)),
+    Clone_Id.
+
+
+%%%===================================================================
+%%% Network transformations 
+%%%===================================================================
+
+% Definition of probabilities
+-define(NETWORK_EXTENSION, 0.10).
+-define(NETWORK_REDUCTION, 0.05).
+-define(SIZE_CONSTANT,     3.00).
+
+%%--------------------------------------------------------------------
+%% @doc Applies the mutations to the network.
+%% @end
+%%--------------------------------------------------------------------
+% TODO: Define spec
+mutate_network(Network) -> 
+    Rate    =  math:sqrt(elements:size(Network)),
+    ?MAYBE(?NETWORK_EXTENSION) extend_network(Network, Rate),
+    ?MAYBE(?NETWORK_REDUCTION) reduce_netkork(Network, Rate),
+    mutate_neurons(Network, Rate).
+
+extend_network(Network, ExtRate) -> 
+    transform:extend_network(elements:id(Network), ExtRate).
+
+reduce_netkork(Network, RedRate) -> 
+    transform:reduce_netkork(elements:id(Network), RedRate).
+
+mutate_neurons(Network, Rate) -> 
+    Prob    = ?SIZE_CONSTANT/Rate,
+    Neurons = ebd:read(ltools:rand(elements:neurons(Network), Prob)),
+    [mutate_neuron(Neuron) || Neuron <- Neurons].
+
+
+%%%===================================================================
+%%% Neuron transformations 
+%%%===================================================================
+
+% Definition of probabilities
+-define(LINKS_EXTENSION,      0.10).
+-define(LINKS_REDUCTION,       0.05).
+-define(ACTIVATION_CHANGE,    0.15).
+-define(AGGREGATION_CHANGE,   0.15).
+-define(BIAS_SWITCH,          0.10).
+-define(FANIN_CONSTANT,       0.50). 
+
+%%--------------------------------------------------------------------
+%% @doc Applies the mutations to the neuron.
+%% @end
+%%--------------------------------------------------------------------
+% TODO: Define specs
+mutate_neuron(Neuron) -> 
+    Rate =  math:sqrt(length(elements:inputs_idps(Neuron))),
+    ?MAYBE(   ?LINKS_EXTENSION) links_extension(Neuron, Rate),
+    ?MAYBE(   ?LINKS_REDUCTION) links_reduction(Neuron, Rate),
+    % ?MAYBE( ?ACTIVATION_CHANGE) activation_change(Neuron),
+    % ?MAYBE(?AGGREGATION_CHANGE) aggregation_change(Neuron),
+    ?MAYBE(       ?BIAS_SWITCH) switch_bias(Neuron),
+    mutate_weights(Neuron).
+
+links_extension(Neuron, ExtRate) -> 
+    transform:extend_connections(elements:id(Neuron), ExtRate).
+
+links_reduction(Neuron, RedRate) -> 
+    transform:reduce_connections(elements:id(Neuron), RedRate).
+
+switch_bias(Neuron) -> 
+    Id = elements:id(Neuron),
+    transform:reset_bias(Id).
+    % case elements:bias(Neuron) of 
+    %     disabled -> transform:disable_bias(Id);
+    %     _        -> transform:reset_bias(Id)
+    % end.
+
+mutate_weights(Neuron) -> 
+    Inputs      = elemens:inputs_ids(Neuron),
+    Probability = ?FANIN_CONSTANT/math:sqrt(length(Inputs)),
+    Selected_Weights = ltools:rand(Inputs, Probability),
+    transform:reset_weights(elements:id(Neuron), Selected_Weights).
+
 
 %%====================================================================
 %% Internal functions
@@ -83,188 +174,19 @@ add_architectures([], _, Sets) ->
     Sets.
 
 
-
-
-
-
-
-
-
-%%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Creates a set of random links between random neurons
-%% - Probability: Probability each link has to be edited/activated
-%% - Recurrence: When false, recurrent links are discarded
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-edit_random_link(Cortex_Id, Probability, #{allow_recurrent_links := RecAllowed}) ->
-    Neurons = nn_elements:neurons(edb:read(Cortex_Id)),
-    Selected_Links = randomSelect([{From_Id, To_Id} || From_Id <- Neurons, To_Id <- Neurons], Probability),
-    if
-        RecAllowed -> [edit_random_link(From_Id, To_Id) || {From_Id, To_Id} <- Selected_Links];
-        true -> [edit_random_link(From_Id, To_Id) || {{L1, _} = From_Id, {L2, _} = To_Id} <- Selected_Links, L1 < L2]
-    end,
-    ok.
-
-edit_random_link(From_Id, To_Id) ->
-    try
-        mutation:create_link(From_Id, To_Id)
-    catch
-        error:{link_fail, _} -> mutation:edit_link(From_Id, To_Id, rand:normal(0, ?DELTA_MULTIPLIER/10.0))
-    end.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Randomly changes the bias of randomly selected neurons
-%% - Probability: Probability a each neuron has to change its bias
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-edit_random_bias(Cortex_Id, Probability, #{}) ->
-    Neurons = nn_elements:neurons(edb:read(Cortex_Id)),
-    Selected_Neurons = randomSelect(Neurons, Probability),
-    [mutation:edit_bias(Id, randomWeightValue()) || Id <- Selected_Neurons],
-    ok.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Changes the activation function of randomly selected neurons
-%% - Probability: Probability that each neuron has to change its af
-%% - Functions_Weights: List of target functions and prob. weight
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-change_to_random_af(Cortex_Id, Probability, #{available_af := Functions_Weights}) ->
-    Neurons = nn_elements:neurons(edb:read(Cortex_Id)),
-    Selected_Neurons = randomSelect(Neurons, Probability),
-    [mutation:change_af(Id, weightSelection(Functions_Weights)) || Id <- Selected_Neurons],
-    ok.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Changes the aggregation function of randomly selected neurons
-%% - Probability: Probability that each neuron has to change its aggrf
-%% - Functions_Weights: List of target functions and prob. weight
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-change_to_random_aggrf(Cortex_Id, Probability, #{available_aggrf := Functions_Weights}) ->
-    Cortex = edb:read(Cortex_Id),
-    Neurons = nn_elements:neurons(Cortex) -- nn_elements:outputs_ids(Cortex),
-    Selected_Neurons = randomSelect(Neurons, Probability),
-    [mutation:change_aggrf(Id, weightSelection(Functions_Weights)) || Id <- Selected_Neurons],
-    ok.
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Add neurons into random layers
-%% - MaxNet_Extension: Maximum % the network can increase
-%% - Activation_FW: List of target af and probability weights
-%% - Aggregation_FW: List of target aggrf and probability weights
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-create_neurons(Cortex_Id, MaxNet_Extension, #{available_af := Activation_FW, available_aggrf := Aggregation_FW}) ->
-    Neurons = nn_elements:neurons(edb:read(Cortex_Id)),
-    ExtensionFactor = length(Neurons) * MaxNet_Extension,
-    Integer_Neurons = floor(ExtensionFactor),
-    Dec = ExtensionFactor - Integer_Neurons,
-    case rand:uniform() of
-        Prob when Prob > Dec -> N_NewNeurons = Integer_Neurons;
-        Prob when Prob =< Dec -> N_NewNeurons = Integer_Neurons + 1
-    end,
-    [mutation:insert_neuron((rand:uniform(198) - 99) / 100,
-                            weightSelection(Activation_FW),
-                            weightSelection(Aggregation_FW),
-                            Cortex_Id)
-     || _ <- lists:seq(1, N_NewNeurons)].
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Remove randomly selected neurons
-%% - MaxNet_Reductions: Maximum % the network can reduce
-%%
-%% @end
-%%--------------------------------------------------------------------
-% TODO: Define specs
-remove_neurons(Cortex_Id, Die_Probability, #{}) ->
-    Neurons = nn_elements:neurons(edb:read(Cortex_Id), hidden),
-    Selected_Neurons = randomSelect(Neurons, Die_Probability),
-    [mutation:remove_neuron(Neuron_Id, Cortex_Id) || Neuron_Id <- Selected_Neurons],
-    ok.
-
-
-%%====================================================================
-%% Internal functions
-%%====================================================================
-
-% ....................................................................
-randomElement(List) -> lists:nth(rand:uniform(length(List)), List).
-
-% ....................................................................
-randomWeightValue() -> ?DELTA_MULTIPLIER * (rand:uniform() - 0.5).
-
-% ....................................................................
-randomSelect(List, Prob) -> [X || X <- List, rand:uniform() < Prob].
-
-% ....................................................................
-randomShuffling(List) -> [X || {_, X} <- lists:sort([{rand:uniform(), N} || N <- List])].
-
-% ....................................................................
-randomFirsts(List, N) -> element(1, lists:split(N, randomShuffling(List))).
-
-% ....................................................................
-weightSelection(FunctionWeight_List) ->
-    List = lists:append([lists:duplicate(W, Function) || {Function, W} <- FunctionWeight_List]),
-    randomElement(List).
-
-
 %%====================================================================
 %% Eunit white box tests
 %%====================================================================
 
 % --------------------------------------------------------------------
 % TESTS DESCRIPTIONS -------------------------------------------------
-this_example_test_() ->
-    % {setup, Where, Setup, Cleanup, Tests | Instantiator}
-    [
-        {"Test example",
-         {setup, local, fun no_setup/0, fun no_cleanup/1, fun test_example/1}}
-    ].
 
 % --------------------------------------------------------------------
 % SPECIFIC SETUP FUNCTIONS -------------------------------------------
-no_setup() ->
-    ok.
-
-no_cleanup(_) ->
-    ok.
 
 % --------------------------------------------------------------------
 % ACTUAL TESTS -------------------------------------------------------
-test_example(_) ->
-    [
-        ?_assertEqual(true, true)
-    ].
-
 
 % --------------------------------------------------------------------
 % SPECIFIC HELPER FUNCTIONS ------------------------------------------
-
-
-
-    
-    
-
-
 
