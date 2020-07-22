3210a0ad33f4e963d99f9afb172ad79debcd1394
BorjaEst <boressan@outlook.com>
2020-07-22 22:51:20 +0200

    New tests, run_as and run

--- test/neurnet_test.exs
+++ test/neurnet_test.exs
@@ -1,5 +1,6 @@
 defmodule NeurnetTest do
   use ExUnit.Case, async: true
+  require Logger
   doctest Neurnet
 
   setup %{} do
@@ -9,32 +10,81 @@ defmodule NeurnetTest do
 
   test "Phenotype from genotype" do
     phenotype = Neurnet.phenotype_from(:dummy_gate)
-    ph_struct = Neurnet.info(phenotype)
+    ph_info = Neurnet.info(phenotype)
 
-    assert [] == ph_struct.actuators -- [:xor_score, :null]
-    assert [] == ph_struct.sensors -- [:seq_1, :seq_2]
-    assert :network == elem(ph_struct.network, 0)
+    assert [] == ph_info.actuators -- [:xor_score, :null]
+    assert [] == ph_info.sensors -- [:seq_1, :seq_2]
+    # assert ph_info.network.nnodes
   end
 
   test "Phenotype mutation" do
     phenotype_1 = Neurnet.phenotype_from(:dummy_gate)
-    ph1_struct = Neurnet.info(phenotype_1)
+    ph1_info = Neurnet.info(phenotype_1)
     phenotype_2 = :eevo.mutate(phenotype_1)
-    ph2_struct = Neurnet.info(phenotype_2)
+    ph2_info = Neurnet.info(phenotype_2)
 
-    assert ph1_struct != ph2_struct
-    assert [] == ph2_struct.actuators -- [:xor_score, :null]
-    assert ph1_struct.sensors == ph2_struct.sensors
-    assert ph1_struct.network != ph2_struct.network
+    assert ph1_info != ph2_info
+    assert [] == ph2_info.actuators -- [:xor_score, :null]
+    assert ph1_info.sensors == ph2_info.sensors
+    assert ph1_info.network != ph2_info.network
   end
 
-  # test "Run training of dummy gate" do
-  #   stop_condition = fn %{runtime: x} -> x > 1000 end
-  #   result = Neurnet.run(:test_dummy, :dummy_gate, 4, stop_condition)
+  test "Run a dummy gate" do
+    phenotype = Neurnet.phenotype_from(:dummy_gate)
+    {:next, function, [state]} = Neurnet.run_as(phenotype)
 
-  #   assert result.population.run_data.runtime >= 1000
-  #   IO.inspect(result.population.run_data)
-  #   assert is_map(result.tree)
-  #   assert map_size(result.tree) > 0
-  # end
+    assert is_function(function)
+    assert is_map(state)
+  end
+
+  test "Run a simple gate" do
+    phenotype = Neurnet.phenotype_from(:simple_gate)
+    {:next, function, [state]} = Neurnet.run_as(phenotype)
+
+    assert is_function(function)
+    assert is_map(state)
+  end
+
+  test "Run a complex gate" do
+    phenotype = Neurnet.phenotype_from(:complex_gate)
+    {:next, function, [state]} = Neurnet.run_as(phenotype)
+
+    assert is_function(function)
+    assert is_map(state)
+  end
+
+  test "Run a broken gate" do
+    phenotype = Neurnet.phenotype_from(:broken_gate)
+    assert_raise(ErlangError, fn -> Neurnet.run_as(phenotype) end)
+  end
+
+  test "Run training of dummy gate" do
+    stop_condition = fn %{generation: x} -> x > 25 end
+    result = Neurnet.run(:test_dummy, :dummy_gate, 4, stop_condition)
+
+    assert result.population.run_data.generation >= 25
+    IO.inspect(result.population.run_data)
+    assert is_map(result.tree)
+    assert map_size(result.tree) > 0
+  end
+
+  test "Training of simple gate" do
+    stop_condition = fn %{generation: x} -> x > 25 end
+    result = Neurnet.run(:test_simple, :simple_gate, 4, stop_condition)
+
+    assert result.population.run_data.generation >= 25
+    IO.inspect(result.population.run_data)
+    assert is_map(result.tree)
+    assert map_size(result.tree) > 0
+  end
+
+  test "Training of complex gate" do
+    stop_condition = fn %{generation: x} -> x > 25 end
+    result = Neurnet.run(:test_complex, :complex_gate, 4, stop_condition)
+
+    assert result.population.run_data.generation >= 25
+    IO.inspect(result.population.run_data)
+    assert is_map(result.tree)
+    assert map_size(result.tree) > 0
+  end
 end
